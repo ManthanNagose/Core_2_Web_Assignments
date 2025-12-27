fdc636964b82ddf280fee469c80ef40da29f586c
Manthan-Nagose <nagosemanthan@gmail.com>
2025-12-27 18:41:19 +0530

    Inheritance files added

---
+++ Practice/Inheritance/Inheritance_1.java
@@ -0,0 +1,29 @@
+class Company{
+    String cmpName;
+    String cmpLoc;
+
+    void cmpInfo () {
+        System.out.println(cmpName);
+        System.out.println(cmpLoc);
+    }
+}
+
+class Employee extends Company {
+    String empName = "Ashish";
+    double empSal = 2.5;
+
+    void empInfo (){
+        System.out.println(empName);
+        System.out.println(empSal);
+    }
+
+}
+
+class Client {
+    public static void main(String [] args){
+        Employee emp = new Employee();
+        emp.empInfo();
+        emp.cmpInfo();
+        
+    }
+}
